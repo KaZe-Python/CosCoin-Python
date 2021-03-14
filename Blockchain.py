55d2abd6ab4b85718f11f720835e3724d0302424
KaZe-Python <bpcosentini239@gmail.com>
2021-03-14 10:28:22 +0000

    PoW implemented

--- Blockchain.py
+++ Blockchain.py
@@ -1,21 +1,20 @@
 from modules.block.Block import Block
 from modules.transaction.Transaction import Transaction
+import time
 
 
 class Blockchain:
     def __init__(self):
         self.blockchain = [self.create_genesis()]
+        self.difficulty = 0
 
     @staticmethod
     def create_genesis() -> Block:
-        return Block(0, 0, "13/03/2021", "genesis", 0000)
-
-    def get_last_block(self) -> Block:
-        return self.blockchain[-1]
+        return Block(0,time.time(), "gen", 0)
 
     def add_block(self, block: Block) -> None:
         block.previousHash = self.get_last_block().hash
-        block.hash = block.calculateBlockHash()
+        block.hash = block.mine_block(self.difficulty)
         self.blockchain.append(block)
 
     def is_chain_valid(self) -> bool:
@@ -23,13 +22,16 @@ class Blockchain:
             current_block = self.blockchain[idx]
             previous_block = self.blockchain[idx-1]
 
-            if current_block.hash != current_block.calculateBlockHash():
+            if current_block.hash != current_block.calculate_blockHash():
                 return False
             
             if current_block.previousHash != previous_block.hash:
                 return False
 
         return True
+
+    def get_last_block(self):
+        return self.blockchain[-1]
     
     def print_chain(self):
         for block in self.blockchain:
@@ -37,6 +39,11 @@ class Blockchain:
 
 
 blockchain = Blockchain()
-blockchain.add_block(Block(1, 0, "14/03/2021", {"amount": 4}))
-blockchain.add_block(Block(2, 0, "15/03/2021", {"amount": 5}))
+print("mining block 1")
+blockchain.add_block(Block(1, "14/03/2021", {"amount": 4}))
+
+print("mining block 2")
+blockchain.add_block(Block(2, "15/03/2021", {"amount": 5}))
+
+blockchain.print_chain()
 print(blockchain.is_chain_valid())
--- modules/block/Block.py
+++ modules/block/Block.py
@@ -3,9 +3,9 @@ import hashlib
 
 
 class Block:
-    def __init__(self, idx: int, nonce: int, timestamp, data: str, previousHash = ""):
+    def __init__(self, idx: int, timestamp, data: str, previousHash = ""):
         self.id = idx
-        self.nonce = nonce
+        self.nonce = 0
         self.timestamp = timestamp
         self.data = data
         self.previousHash = previousHash
@@ -14,10 +14,19 @@ class Block:
                 + str(self.timestamp) + str(self.data) \
                 + str(self.previousHash)).encode()
 
-        self.hash = self.calculateBlockHash()
+        self.hash = self.calculate_blockHash()
 
-    def calculateBlockHash(self):
+    def calculate_blockHash(self):
         return hashlib.sha256(self.datahash).hexdigest()
+
+    def mine_block(self, difficulty):
+        zeros_list = [str(0) for _ in range(difficulty)]
+
+        while(list(self.hash[:difficulty]) != zeros_list):
+            self.nonce += 1
+            self.hash = self.calculate_blockHash()
+        return self.hash
+
 
     def __repr__(self):
         return f"##### \n\
